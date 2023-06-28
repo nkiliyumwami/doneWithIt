0399b879797681261bd81e6ed2fa7b07d20da12d
Emmanuel Nkiriyumwami <nkiliyumwami@gmail.com>
2023-06-27 20:04:36 -0400

    Forms

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import Card from "./app/components/Card";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  return <LoginScreen />;
 }
